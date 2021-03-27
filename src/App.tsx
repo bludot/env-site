6a756d947444a46457479dc8bf091581e81cf868
James <admin@floretos.com>
2021-03-27 13:11:27 +0700

    circleci fix

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <code style={{ background: "#ccc", padding: "0.5em" }}>{
+        <code style={{ background: "rgb(30, 30, 30)", padding: "0.5em" }}>{
           JSON.stringify(window.CONFIG, null, 2)
         }</code>
       </header>
